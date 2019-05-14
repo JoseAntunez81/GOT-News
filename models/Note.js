7e97f4c9b3d4329358d4cfde2f4b4d1d542c2b97
Jose Antunez <jangel5673@yahoo.com>
2019-05-14 15:00:23 -0700

    Update

---
+++ models/Note.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new NoteSchema object
+// This is similar to a Sequelize model
+var noteSchema = new Schema({
+  // The headline is the article associate with the note
+  _headlineId: {
+    type: Schema.Types.ObjectId,
+    ref: "Headline"
+  },
+  // date is just a string
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  // note text is astring as well
+  noteText: String
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Note = mongoose.model("Note", noteSchema);
+
+// Export the Note model
+module.exports = Note;
